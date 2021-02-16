78f65d0c467823987368cc1b8a6d80e5210945fa
Phil <pjguitar15@gmail.com>
2021-02-16 11:10:22 +0800

    changed logo styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="Navbar">
-      <Link to='/'>
-        <a href="./Home.jsx" style={{ position: 'absolute' }} className="mx-5 logo">Philcob Guitar Shop</a>
-      </Link>
+      <div className="text-xl-start mb-3">
+        <Link to="/">
+          <a href="./Home.jsx" className="col-sm-12 mx-5 logo">
+            Philcob Guitar Shop
+          </a>
+        </Link>
+      </div>
       <div className="Icons text-center">
-        <a href="#" className="mx-5">GEAR</a>
-        <a href="#" className="mx-5">CUSTOM</a>
-        <Link to='/cart'>
-          <a href="./Cart.jsx" className="mx-5">CART</a>
+        <a href="#" className="mx-5">
+          GEAR
+        </a>
+        <a href="#" className="mx-5">
+          CUSTOM
+        </a>
+        <Link to="/cart">
+          <a href="./Cart.jsx" className="mx-5">
+            CART
+          </a>
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
